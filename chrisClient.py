27be1c617c4cae3fe6c01869909282fb2ac20583
Sandip Samal <sandip.samal@childrens.harvard.edu>
2024-11-07 15:54:30 -0500

    added CLI params

---
+++ chrisClient.py
@@ -0,0 +1,69 @@
+### Python Chris Client Implementation ###
+
+from base_client import BaseClient
+from chrisclient import client
+import json
+
+class ChrisClient(BaseClient):
+    def __init__(self, url: str, username: str, password: str):
+        self.cl = client.Client(url, username, password)
+        self.cl.pacs_series_url = "http://localhost:8000/api/v1/pacs/series/"
+
+    def create_con(self,params:dict):
+        return self.cl
+
+    def pacs_pull(self):
+        pass
+    def pacs_push(self):
+        pass
+
+    def anonymize(self, params: dict):
+        prefix = "dynanon"
+        feed_name = self.__create_feed_name(prefix,params["search"])
+        # search for dicom dir
+        dicom_dir = self.__get_dir_path(params["search"])
+        anon_params = json.dumps(params["anon"])
+
+        # run dircopy
+        pl_id = self.__get_plugin_id({"name":"pl-dsdircopy","version":"1.0.2"})
+        pv_in_id = self.__create_feed(pl_id,{"previous_id":1,'dir':dicom_dir,'title':feed_name})
+        # run dicom_headeredit
+        pl_sub_id = self.__get_plugin_id({"name":"pl-pfdicom_tagsub", "version":"3.2.4"})
+        data = {"previous_id": pv_in_id, "tagStruct": anon_params, 'fileFilter': '.dcm'}
+        self.__create_feed(pl_sub_id, data)
+        pass
+
+    def __create_feed(self, plugin_id: str,params: dict):
+        response = self.cl.create_plugin_instance(plugin_id, params)
+        return response['id']
+
+    def __create_feed_name(self, prefix: str, params: dict) -> str:
+        name = ""
+        for val in params.values():
+            name += f"-{val}"
+        return  f"{prefix}{name}"
+
+    def __get_plugin_id(self, params: dict):
+        response = self.cl.get_plugins(params)
+        if response['total'] > 0:
+            return response['data'][0]['id']
+        raise Exception(f"No plugin found with matching search criteria {params}")
+
+    def __get_dir_path(self, params: dict):
+        mylist = []
+        for key in params.keys():
+            mylist.append(params[key])
+        #{"fname_icontains": mylist[0]})
+        files = self.cl.get_pacs_series_list(params)
+        print(files)
+        l_dir_path = set()
+        for file in files:
+            print(file)
+        # for file in files['links']:
+        #     file_path = file['fname']
+        #     file_name = file_path.split('/')[-1]
+        #     dir_path = file_path.replace(file_name, '')
+        #     l_dir_path.add(dir_path)
+        # return ','.join(l_dir_path)
+
+
--- dyanon.py
+++ dyanon.py
@@ -4,6 +4,11 @@ from pathlib import Path
 from argparse import ArgumentParser, Namespace, ArgumentDefaultsHelpFormatter
 
 from chris_plugin import chris_plugin, PathMapper
+import pandas as pd
+import json
+import itertools
+from collections import ChainMap
+from chrisClient import ChrisClient
 
 __version__ = '1.0.0'
 
@@ -29,6 +34,28 @@ parser.add_argument('-p', '--pattern', default='**/*.txt', type=str,
                     help='input file filter glob')
 parser.add_argument('-V', '--version', action='version',
                     version=f'%(prog)s {__version__}')
+parser.add_argument(
+    "--pattern",
+    default="**/*dcm",
+    help="""
+            pattern for file names to include (you should quote this!)
+            (this flag triggers the PathMapper on the inputdir).""",
+)
+parser.add_argument(
+    "--pluginInstanceID",
+    default="",
+    help="plugin instance ID from which to start analysis",
+)
+parser.add_argument(
+    "--CUBEurl", default="http://localhost:8000/api/v1/", help="CUBE URL"
+)
+parser.add_argument("--CUBEuser", default="chris", help="CUBE/ChRIS username")
+parser.add_argument("--CUBEpassword", default="chris1234", help="CUBE/ChRIS password")
+parser.add_argument(
+    "--pftelDB",
+    help="an optional pftel telemetry logger, of form '<pftelURL>/api/v1/<object>/<collection>/<event>'",
+    default="",
+)
 
 
 # The main function of this *ChRIS* plugin is denoted by this ``@chris_plugin`` "decorator."
@@ -42,6 +69,9 @@ parser.add_argument('-V', '--version', action='version',
     min_memory_limit='100Mi',    # supported units: Mi, Gi
     min_cpu_limit='1000m',       # millicores, e.g. "1000m" = 1 CPU core
     min_gpu_limit=0              # set min_gpu_limit=1 to enable GPU
+)
+@pflog.tel_logTime(
+    event="dyanon", log="Dynamic Anonymization of DICOMs"
 )
 def main(options: Namespace, inputdir: Path, outputdir: Path):
     """
@@ -75,3 +105,49 @@ def main(options: Namespace, inputdir: Path, outputdir: Path):
 
 if __name__ == '__main__':
     main()
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def create_query(df: pd.DataFrame, str_srch_idx: str, str_anon_idx: str):
+    l_srch_idx = list(map(int,str_srch_idx.split(',')))
+    l_anon_idx = list(map(int,str_anon_idx.split(',')))
+
+    # create connection object
+    cube_con = ChrisClient("http://localhost:8000/api/v1/", "chris", "chris1234")
+
+    for row in df.iterrows():
+        d_job = {}
+        s_col = (df.columns[l_srch_idx].values)
+        s_row = (row[1][l_srch_idx].values)
+        s_d = [{k: v} for k, v in zip(s_col, s_row)]
+
+        d_job["search"] = dict(ChainMap(*s_d))
+        a_col=(df.columns[l_anon_idx].values)
+        a_row=(row[1][l_anon_idx].values)
+        a_d = [{k.split('.')[0]:v} for k,v in zip(a_col,a_row)]
+        d_job["anon"] = dict(ChainMap(*a_d))
+
+        print(d_job)
+        submit_job(cube_con, d_job)
+
+
+
+def serialize_csv(file_path: str, anon_all: bool):
+    df = pd.read_csv(file_path)
+    search_idx = "1"
+    anon_id = "0,2,3,4"
+    create_query(df,search_idx,anon_id)
+
+
+def submit_job(con: ChrisClient, job: dict):
+    con.anonymize(job)
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    file_path: str ="./sample3.csv"
+    anon_all: bool = False
+    serialize_csv(file_path, anon_all)
+
